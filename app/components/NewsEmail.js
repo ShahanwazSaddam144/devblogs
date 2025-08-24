2b0e7efbc76eeb72355b05375fd7a538ab36a113
='coder101_js <='op422010@gmail.com>
2025-08-25 01:40:30 +0500

    bug fixes/oAuth user error

--- app/components/NewsEmail.js
+++ app/components/NewsEmail.js
@@ -14,7 +14,7 @@ const NewsEmail = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/newsemail", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/newsemail`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
